14a424ca4fbf613a7bf997cbaca37af4e97b8236
Francesco Zaia <francesco.zaia@gmail.com>
2013-04-20 22:33:06 +0100

    removed tween.js, css transitions are ok

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,25 @@
 (function(window, undefined){
-	var navigation = responsiveNav("#nav", {customToggle: "#toggle"});
 
-	
+	"use strict";
 
  	var init = function() {
 
 		ImagePreloader.moduleMethod("ole");
 		var photo = document.querySelector(".gallerySlider");
-		var tween = new TWEEN.Tween( { scrollLeft: 50, y: 0 } )
-		    .to( { scrollLeft: -1200 }, 2000 )
-		    .easing( TWEEN.Easing.Quartic.InOut )
-		    .onUpdate( function () {
 
-		        //photo.style.scrollLeft = this.x + 'px';
-
-		    } )
-		    .start();
-	}
-
-	var animate = function() {
-	    window.requestAnimationFrame( animate ); // js/RequestAnimationFrame.js needs to be included too.
-	    //TWEEN.update();
+        window.addEventListener('load', function() {
+			var m = new Delegate(document.body);
+			m.on("click", "#toggle", function(){
+				if(document.getElementsByTagName("nav")[0].className.split(" ").indexOf("opened") > -1) {
+					document.getElementsByTagName("nav")[0].className = "";
+				} else {
+					document.getElementsByTagName("nav")[0].className = "opened";
+				}
+				
+			})
+		}, false);
 	}
 
 	init();
-	animate();
 
 })(this);
