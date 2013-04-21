d7a1c9829706d7c4e411b73d7c3ca09c85612998
Francesco Zaia <francesco.zaia@gmail.com>
2013-04-21 18:04:43 +0100

    CSS clean - started

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,11 @@
 
 	"use strict";
 
- 	var init = function() {
-
-		//ImagePreloader.moduleMethod("ole");
-		//var photo = document.querySelector(".gallerySlider");
-
-        window.addEventListener('load', function() {
-			var m = new Delegate(document.body);
-			m.on("click", ".toggleButton", function(){
-				var element = document.getElementsByTagName("section")[0];
-				if(element.className.split(" ").indexOf("opened") > -1) {
-					element.className = "";
-				} else {
-					element.className = "opened";
-				}
-				
-			})
-		}, false);
-	}
-
-	init();
+	// [TODO: avoid window pollution]
+ 	var m = new Delegate(document.body);
+	m.on("click", ".toggleButton", function() {
+		var element = document.getElementsByTagName("section")[0];
+		element.className = (element.className.split(" ").indexOf("opened") > -1) ? "" : "opened";
+	})
 
 })(this);
