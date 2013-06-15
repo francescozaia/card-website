92d5f2d2d852d8a323bdf249106fb8e6a4e58e33
Francesco Zaia <francesco.zaia@gmail.com>
2013-06-15 13:11:43 +0100

    useragents way...

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 (function (window) {
   "use strict";
-  var delegate = new Delegate(document.body);
-  delegate.on("click", ".toggleButton", function () {
-    var element = document.getElementsByTagName("section")[0];
-    element.className = (element.className.split(" ").indexOf("opened") > -1) ? "" : "opened";
-  });
+// var delegate = new Delegate(document.body);
+// delegate.on("click", ".toggleButton", function () {
+// var element = document.getElementsByTagName("section")[0];
+// element.className = (element.className.split(" ").indexOf("opened") > -1) ? "" : "opened";
+// });
+	alert("mobile");
 }(this));
