5e89afd98baab7a18cb6287595c36a65470ce443
Francesco Zaia <francesco.zaia@gmail.com>
2013-06-15 16:38:22 +0100

    small refactoring html+css

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  "use strict";
-// var delegate = new Delegate(document.body);
-// delegate.on("click", ".toggleButton", function () {
-// var element = document.getElementsByTagName("section")[0];
-// element.className = (element.className.split(" ").indexOf("opened") > -1) ? "" : "opened";
-// });
-	alert("mobile");
-}(this));
